1cec6bad89de592519c6f388ef30d6f72a0731a2
takumi0706 <ganndamu0706@gmail.com>
2024-06-03 12:29:33 +0900

    feat:add springboot(spring boot DevTools, Lombok, Spring Web, Thymeleaf

---
+++ src/main/java/com/example/hackthon_vol6_team/HackthonVol6TeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackthon_vol6_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackthonVol6TeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackthonVol6TeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hackthon_vol6_team/HackthonVol6TeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackthon_vol6_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackthonVol6TeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
